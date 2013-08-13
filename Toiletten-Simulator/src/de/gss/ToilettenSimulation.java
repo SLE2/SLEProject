c39ae7bf8d57f7a8aa11029af8801ac0b2b2fcee
kracht <0kracht@informatik.uni-hamburg.de>
2013-08-13 15:21:58 +0200

    cccccccc

--- Toiletten-Simulator/src/de/gss/ToilettenSimulation.java
+++ Toiletten-Simulator/src/de/gss/ToilettenSimulation.java
@@ -11,7 +11,7 @@ import de.gss.toilette.*;
  * In dieser Klasse wird die Implemntierung der virtuellen Toilette festgelegt und 
  * es werden die Threads der virtuellen Personen erzeugt, initialisiert und gestartet.
  * ghgfgfhsdfsdfsdfsxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxsdddss
- * reregreg333322sdrtgggtrtgtrsssss
+ * reregreg333322sdrtgggtrtgtrsssssfgggg
  * @author bade
  */
 public class ToilettenSimulation
