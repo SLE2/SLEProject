b074d33c1d87ab422611f22994b6ba795b0519bf
kracht <0kracht@informatik.uni-hamburg.de>
2013-08-14 11:41:34 +0200

    y

--- Toiletten-Simulator/src/de/gss/ToilettenSimulation.java
+++ Toiletten-Simulator/src/de/gss/ToilettenSimulation.java
@@ -13,7 +13,7 @@ import de.gss.toilette.*;
  * es werden die Threads der virtuellen Personen erzeugt, initialisiert und gestartet.
  * ghgfgfhsdfsdfsdfsxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxsdddss
  * reregreg333322sdrtgggtrtgtrsssssfggggyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
- * zzzzzzzzzzzzzzzzzzxdjjj2468165421874sfzurebuzrebrb
+ * zzzzzzzzzzzzzzzzzzxdjjj2468165421874sfzurebuzrebrby
  * @author bade
  */
 public class ToilettenSimulation
